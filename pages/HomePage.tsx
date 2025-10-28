a5de920f1713f0f9e4b708e8ffee8aa34716ccaf
Sanjay <sanjaygunabalan@Sanjays-MacBook-Air.local>
2025-10-28 23:09:19 +0800

    Revert "Remove 3D carousel from homepage"
    This reverts commit 40854f078eb2ada8d3695f8b10ea0a1cd936ebc7.

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Header from '../components/public/Header';
 import Hero from '../components/public/Hero';
 import Services from '../components/public/Services';
 import TechnologyShowcase from '../components/public/TechnologyShowcase';
+import Portfolio3D from '../components/public/Portfolio3D';
 import Testimonials from '../components/public/Testimonials';
 import Footer from '../components/public/Footer';
 
@@ -14,6 +15,7 @@ const HomePage: React.FC = () => {
         <Hero />
         <Services />
         <TechnologyShowcase />
+        <Portfolio3D />
         <Testimonials />
       </main>
       <Footer />
